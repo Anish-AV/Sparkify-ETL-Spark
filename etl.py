a0415c53282d9ed88ebbce46a40cf9ad3fe192a9
Anish <anish.viswanathan@dxc.com>
2021-04-20 00:38:01 +0100

    process song data

--- etl.py
+++ etl.py
@@ -2,6 +2,7 @@ import pyspark
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType, IntegerType
 
 
 def create_spark_session():
@@ -9,7 +10,7 @@ def create_spark_session():
     return spark
 
 
-def process_song_data(spark,input_data, output_data):
+def process_song_data(spark,input_data,output_data):
     song_data = os.path.join(input_data,'song_data/*/*/*/*.json')
     song_schema = StructType([
         StructField("artist_id", StringType()),
@@ -23,5 +24,10 @@ def process_song_data(spark,input_data, output_data):
         StructField("year", IntegerType()),
     ])
     df = spark.read.json(song_data,schema = song_schema)
-
+    songs_fields=['title','artist_id','year','duration']
+    songs_table= df.select(songs_fields).dropDuplicates().withColumn("song_id",monotonically_increasing_id())
+    songs_table.write.mode("overwrite").parquet(output_data + 'songs')
     
+    artists_fields=['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']
+    artists_table=df.select(artists_fields).dropDuplicates()
+    artists_table.write.mode("overwrite").parquet(output_data + 'artists')
